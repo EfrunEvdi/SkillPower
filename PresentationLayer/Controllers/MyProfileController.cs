b37eca88d13ee886dca4bfee6044e00883eafe1c
efrunevdi <efruncetkin@gmail.com>
2023-06-03 18:09:47 +0300

    4. Hafta Sonu

--- PresentationLayer/Controllers/MyProfileController.cs
+++ PresentationLayer/Controllers/MyProfileController.cs
@@ -1,12 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DtoLayer.Dtos.AppUserDtos;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace PresentationLayer.Controllers
 {
+    [Authorize]
     public class MyProfileController : Controller
     {
-        public IActionResult Index()
+        private readonly UserManager<AppUser> _userManager;
+
+        public MyProfileController(UserManager<AppUser> userManager)
         {
-            return View();
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var values = await _userManager.FindByNameAsync(User.Identity.Name);
+            AppUserEditDto appUserEditDto = new AppUserEditDto();
+            appUserEditDto.Name = values.Name;
+            appUserEditDto.Surname = values.Surname;
+            appUserEditDto.PhoneNumber = values.PhoneNumber;
+            appUserEditDto.ImageUrl = values.ImageUrl;
+            appUserEditDto.Email = values.Email;
+
+            TempData["Name"] = appUserEditDto.Name;
+            TempData["Surname"] = appUserEditDto.Surname;
+            TempData["Image"] = appUserEditDto.ImageUrl;
+
+            return View(appUserEditDto);
         }
     }
 }
---
+++ PresentationLayer/ViewComponents/Customer/_CustomerLayoutNavbarPartial.cs
@@ -0,0 +1,14 @@
+﻿using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutNavbarPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
